ce253a19937eefb51e5ca1f5f731f5b2be8a08d2
Andrew <ads739912@gmail.com>
2022-07-26 14:11:55 +0300

    finished console app

--- js/training.js
+++ js/training.js
@@ -401,19 +401,52 @@
 //   }
 
 
-const box = document.getElementById('box');
-console.log(box);
+// const box = document.getElementById('box'),
+//       btns = document.getElementsByTagName('button'),
+//       circles = document.getElementsByClassName('circle'),
+//       hearts = document.querySelectorAll('.heart'),
+//       oneHeart = document.querySelector('.heart'),
+//       wrapper = document.querySelector('.wrapper');
 
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
+// // box.style.backgroundColor = 'green';
+// // box.style.width = '500px';
 
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
+// box.style.cssText = 'background-color: blue; width: 500px';
 
-const hearts = document.querySelectorAll('.heart');
-hearts.forEach(item => {
-  console.log(item);
-});
+// btns[1].style.borderRadius = '100%';
+// circles[0].style.backgroundColor = 'red';
 
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+// for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'blue';
+// }
+
+// hearts.forEach(item => {
+//   item.style.backgroundColor = 'blue';
+// });
+
+// const div = document.createElement('div');
+// // const text = document.createTextNode('тут был я');
+
+// div.classList.add('black');
+
+// // wrapper.append(div);
+// // wrapper.appendChild(div);
+
+// // wrapper.prepend(div);
+
+// // hearts[0].before(div);
+// // hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]);
+
+// // circles[0].remove();
+// // wrapper.removeChild(hearts[1]);
+
+// // hearts[0].replaceWith(circles[0]);
+// // wrapper.replaceChild(circles[0], hearts[0]);
+
+// // div.innerHTML = "<h1>Hello World</h1>";
+
+// div.textContent = "hello";
+
+// div.insertAdjacentHTML("afterend", '<h2>Hello</h2>');
